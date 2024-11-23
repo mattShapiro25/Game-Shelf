2d0bf682f354a5185302fb2f5e5fa4c56b5e41b1
“Sebyzz007 <sdruehl@colgate.edu>
2024-11-23 14:59:23 -0500

    styling done, add turbo

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -7,6 +7,11 @@ class Friend < ApplicationRecord
     create!(user_id1: user1.id, user_id2: user2.id)
     create!(user_id1: user2.id, user_id2: user1.id)
   end
+
+  def self.destroy_bidirectional_friend(user1, user2)
+    user1.friends.delete(user2)
+    user2.friends.delete(user1)
+  end
   
   #ensures only unique friendships can be created
   validates :user_id1, uniqueness: { scope: :user_id2 }
