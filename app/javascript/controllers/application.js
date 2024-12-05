cc29b639a484899e4efdc26e2dba045890f3c303
“Beckman123” <wbeckhorn@colgate.edu>
2024-12-04 22:17:11 -0500

    actually rendered correct form edits

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,7 +1,8 @@
 import { Application } from "@hotwired/stimulus"
+import { Turbo } from "@hotwired/turbo-rails"
 
 const application = Application.start()
-
+Turbo.start()
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
